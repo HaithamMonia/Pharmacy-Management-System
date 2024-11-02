52fc5a6cf240bf7cfa1734c4afc7ffe604c18ea0
mohamed <altenenaccs@gmail.com>
2024-11-02 12:29:00 +0300

    added comments to the login

--- Pharamcy/src/Login.java
+++ Pharamcy/src/Login.java
@@ -3,15 +3,32 @@ import java.io.PrintWriter;
 import java.rmi.server.ExportException;
 import java.util.Scanner;
 
+/**
+ * This class represents a login system for a pharmacy application.
+ */
 public class Login {
     private String username;
     private String password;
     private Scanner scan = new Scanner(System.in);
+
+    /**
+     * Constructs a Login object with the given username and password.
+     *
+     * @param username The username for the login.
+     * @param password The password for the login.
+     */
     public Login(String username, String password){
        this.username = username;
        this.password = password;
     }
 
+    /**
+     * Validates a user by checking if the given username and password match the ones stored in the "users.txt" file.
+     *
+     * @param un The username to validate.
+     * @param pw The password to validate.
+     * @return True if the username and password match, false otherwise.
+     */
     public boolean validateUser(String un,String pw){
 
         try {
